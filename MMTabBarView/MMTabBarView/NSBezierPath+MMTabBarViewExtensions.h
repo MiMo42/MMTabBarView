f666ba8b1d04b5bf2cca8191f65d28075504fb09
Michael Monscheuer <michi@monscheuer.de>
2012-09-26 16:57:22 +0200

    New category on NSAffineTransform
    new method:
    -mm_flipVertical:

---
+++ MMTabBarView/MMTabBarView/NSBezierPath+MMTabBarViewExtensions.h
@@ -0,0 +1,13 @@
+//
+//  NSBezierPath+MMTabBarViewExtensions.h
+//  MMTabBarView
+//
+//  Created by Michael Monscheuer on 9/26/12.
+//  Copyright (c) 2012 Michael Monscheuer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSBezierPath (MMTabBarViewExtensions)
+
+@end
---
+++ MMTabBarView/MMTabBarView/NSBezierPath+MMTabBarViewExtensions.m
@@ -0,0 +1,13 @@
+//
+//  NSBezierPath+MMTabBarViewExtensions.m
+//  MMTabBarView
+//
+//  Created by Michael Monscheuer on 9/26/12.
+//  Copyright (c) 2012 Michael Monscheuer. All rights reserved.
+//
+
+#import "NSBezierPath+MMTabBarViewExtensions.h"
+
+@implementation NSBezierPath (MMTabBarViewExtensions)
+
+@end
