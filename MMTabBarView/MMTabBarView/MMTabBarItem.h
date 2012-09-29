dee55d78ec222141c7b7fb2ecc8066b890f63fc7
Michael Monscheuer <michi@monscheuer.de>
2012-09-29 16:41:16 +0200

    New category on NSTabViewItem
    New category provides new additional properties:
    largeImage
    icon
    isProcessing
    objectCount
    objectCountColor
    isEdited
    hasCloseButton
    (we use Associative References for that)

---
+++ MMTabBarView/MMTabBarView/MMTabBarItem.h
@@ -0,0 +1,13 @@
+//
+//  MMTabBarItem.h
+//  MMTabBarView
+//
+//  Created by Michael Monscheuer on 9/29/12.
+//  Copyright (c) 2012 Michael Monscheuer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MMTabBarItem <NSObject>
+
+@end
