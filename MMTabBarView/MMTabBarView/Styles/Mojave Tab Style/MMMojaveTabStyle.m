a443008d901bf4c27ef13b71081e9b9a698053ee
Steven Green <steven@greenius.ltd.uk>
2020-09-21 20:27:17 +0100

    Fix problem of assigned controlView being optimised away in XCode 12 causing zombie crashes

--- MMTabBarView/MMTabBarView/Styles/Mojave Tab Style/MMMojaveTabStyle.m
+++ MMTabBarView/MMTabBarView/Styles/Mojave Tab Style/MMMojaveTabStyle.m
@@ -161,7 +161,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 	 // Figure out correct text color
     
-    if ( cell.controlView.state == NSOnState )
+    if ( cell.tabBarButton.state == NSOnState )
     {
         textColor = [self colorForPart:MMMtabSelectedFont ofTabBarView:cell.tabBarView];
     }
